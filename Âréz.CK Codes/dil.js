4de521dc6c56b888329300423bf1d9291ace2f61
arezck <81039159+arezck@users.noreply.github.com>
2021-03-20 22:02:11 +0300

    Delete dil.js

--- Âréz.CK Codes/dil.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-const Database = require('plasma-db');
-const db = new Database("./database.json");
-exports.run = message => {}
-exports.conf = {aliases: [], permlvl: 0}
-exports.help = {name: "dil"}
